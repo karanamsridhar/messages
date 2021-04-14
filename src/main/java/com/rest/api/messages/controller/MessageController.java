10dcc26d99d59af5e42b9337d443853fddf4b379
karanamsridhar <karanamsridhar@gmail.com>
2021-04-13 23:28:56 -0500

    Initial Commit

---
+++ src/main/java/com/rest/api/messages/controller/MessageController.java
@@ -0,0 +1,14 @@
+package com.rest.api.messages.controller;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class MessageController {
+
+	@GetMapping("/hello/{name}")
+	public String getMessage(@PathVariable(name="name")String name) {
+		return "Hello, "+name;
+	}
+}
